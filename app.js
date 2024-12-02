ce6fae2a07016911a51b9acad88d6b7d981f223a
alryyan1 <alryyan50@gmail.com>
2024-12-03 01:09:07 +0400

    'shift'

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
 import { Socket } from 'engine.io'
-import {Server} from 'socket.io'
-const io = new Server(3000,{
-    cors:{
-        origin : ['http://localhost:5173']
-        
+import { Server } from 'socket.io'
+const io = new Server(3000, {
+    cors: {
+        origin: ['http://localhost:5173']
+
         // origin : ['http://localhost:5173','http://192.168.1.5:8080']
     }
 })
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('client connected successfully')
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log('client disconnected')
     })
-    socket.on('labPayment',(patient)=>{
-        console.log('labPaymentFromServer '+ patient)
+    socket.on('labPayment', (patient) => {
+        console.log('labPaymentFromServer ' + patient)
 
-        io.emit('labPaymentFromServer',patient)
+        io.emit('labPaymentFromServer', patient)
     })
-    socket.on('patientUpdated',(patient)=>{
-        console.log('patientUpdated '+ patient)
+    socket.on('patientUpdated', (patient) => {
+        console.log('patientUpdated ' + patient)
 
-        io.emit('patientUpdatedFromServer',patient)
+        io.emit('patientUpdatedFromServer', patient)
     })
-    socket.on('newDoctorPatient',(patient)=>{
-        console.log('new incoming patient '+ patient.patient.name)
+    socket.on('newDoctorPatient', (patient) => {
+        console.log('new incoming patient ' + patient.patient.name)
 
-        io.emit('newDoctorPatientFromServer',patient)
-    })
-
-    socket.on('lab_request_confirm',(patient)=>{
-        console.log( 'labrRquestConfirmFromServer '+ patient)
-
-        io.emit('labrRquestConfirmFromServer',patient)
+        io.emit('newDoctorPatientFromServer', patient)
     })
 
+    socket.on('newShift', () => {
+        console.log('new incoming shift ' )
 
-    socket.on('resultAuthenticated',(patient)=>{
-        console.log('resultAuthenticated for patient '+ patient)
+        io.emit('newShiftOpenedFromServer')
+    })
+    socket.on('lab_request_confirm', (patient) => {
+        console.log('labrRquestConfirmFromServer ' + patient)
 
-        io.emit('authenticatedResult',patient)
+        io.emit('labrRquestConfirmFromServer', patient)
+    })
+
+
+    socket.on('resultAuthenticated', (patient) => {
+        console.log('resultAuthenticated for patient ' + patient)
+
+        io.emit('authenticatedResult', patient)
     })
 })
 
